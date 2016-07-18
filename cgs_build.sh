50466c9227dd3ba204b191c6bcb08117a8852bc1
pushyami <pushyami@umich.edu>
2016-07-18 10:51:27 -0400

    Adding the build.sh for buils purposes

---
+++ cgs_build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh --
+## find the next Jenkins build number from environment setting: BUILD_NUMBER
+## if missing, set the number to be 1
+echo "shell env variable BUILD_NUMBER is ${BUILD_NUMBER}"
+build_number=${BUILD_NUMBER:=1}
+
+## check the artifact folder, whether it exists or not
+if [ ! -d "artifact" ]; then
+    ## no artifact folder
+    ## create such folder first
+    echo "create artifact folder"
+    mkdir artifact
+    chmod -R 755 artifact
+fi
+echo "Generating zip file for build number: ${build_number}"
+## get git hex number
+gitNum=`git log -n 1 --pretty="format:%h"`
+printf 'github version number is %s.' $gitNum > git_version.txt
+find . -maxdepth 1 -type f \( -name "*_version.txt" -o -name "*.py" \) -exec tar -rf ./artifact/CGS_$build_number_$gitNum.tar {} \;
+
+
